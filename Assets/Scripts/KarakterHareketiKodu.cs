17394b754661052d715bcfc272154dd0dd7f276a
selamisafa <selamisafa1999@hotmail.com>
2021-12-01 22:05:16 +0300

    Ders 2

--- Assets/Scripts/KarakterHareketiKodu.cs
+++ Assets/Scripts/KarakterHareketiKodu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,8 +9,16 @@ public class KarakterHareketiKodu : MonoBehaviour
     // Karakterimize girdiye bağlı olarak bir güç uygulamak
 
     public Rigidbody2D rb;
-    public float ziplamaGucu;
     
+    public float ziplamaGucu;
+    public float hareketHizi;
+    
+    public Animator anim;
+    public SpriteRenderer renderer;
+
+    public Transform yerKontrolu;
+
+    public AudioSource jump;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,12 +29,62 @@ public class KarakterHareketiKodu : MonoBehaviour
     void Update()
     {
         float h = Input.GetAxis("Horizontal");
-        
-        rb.velocity = Vector2.right * h;
 
+        rb.velocity = new Vector2(h * hareketHizi, rb.velocity.y);
+
+        if (h != 0)
+        {
+            if (h > 0)
+            {
+                renderer.flipX = false;
+            }
+            else
+            {
+                renderer.flipX = true;
+            }
+            anim.SetBool("isWalking", true);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(Vector2.up * ziplamaGucu);
+            if (Physics2D.Raycast(yerKontrolu.position, Vector2.down, 0.1f))
+            {
+                jump.Play();
+                anim.SetBool("onAir", true);
+
+                rb.AddForce(Vector2.up * ziplamaGucu);
+            }
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        anim.SetBool("onAir", false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Anahtar")
+        {
+            Debug.Log("anahtar toplandi!");
+            
+            Destroy(other.gameObject);
+        }
+        
+        if (other.tag == "Kalp")
+        {
+            Debug.Log("Kalp toplandi!");
+            
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(yerKontrolu.position, Vector2.down* 0.1f);
     }
 }
